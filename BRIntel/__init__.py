682d30834b98815101782b47e31d5c323334a675
jul10l1r4 <jul10l1r4@disroot.org>
2023-01-27 14:34:20 -0300

    :pick: Fixing list

--- BRIntel/__init__.py
+++ BRIntel/__init__.py
@@ -2,5 +2,16 @@ import BRIntel.xfe
 import BRIntel.otx
 
 # Get all blob and make a list
-def geral():
-    pass
+def allSources(term):
+    data = []
+    data = BRIntel.xfe.search(term)
+    data += BRIntel.otx.search(term)
+    return data
+
+def reduced(term):
+    # Fazer uns loop com todos os valores para entregar
+    # os dados reduzidos com url em cada
+    # id, name, description, author, date: created and modified ...
+    # url, TLP
+    return False
+ 
