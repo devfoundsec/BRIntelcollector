f899d548cf074c48fefac092b9ec7a4800a43985
jul10l1r4 <jul10l1r4@disroot.org>
2023-01-27 14:40:24 -0300

    :rocket: removing unless code

--- BRIntel/xfe/__init__.py
+++ BRIntel/xfe/__init__.py
@@ -5,14 +5,5 @@ from dotenv import load_dotenv
 load_dotenv()
 IBM_KEY = environ['IBM_KEY']
 
-def bigDetails(term):
-
 def search(term):
     return grabAll(f"{term} (brasil or brazil)", IBM_KEY)
-
-def reduced(term):
-    # Fazer uns loop com todos os valores para entregar
-    # os dados reduzidos com url em cada
-    # id, name, description, author, date: created and modified ...
-    # url, TLP
-    
