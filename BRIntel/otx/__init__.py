6d7f7fb97c6bbc3b646d58d543a6a28aa1239b8a
jul10l1r4 <jul10l1r4@disroot.org>
2023-01-29 23:51:33 -0300

    :pick: Add show function for return default value of otx source

--- BRIntel/otx/__init__.py
+++ BRIntel/otx/__init__.py
@@ -6,8 +6,19 @@ from OTXv2 import OTXv2
 load_dotenv()
 otx = OTXv2(environ["OTX_KEY"])
 
+def show(pulse):
+    data = {}
+    data["title"] = pulse["name"]
+    data["description"] = pulse["description"]
+    data["author"] = pulse["author_name"]
+    data["created"] = pulse["created"]
+    data["modified"] = pulse["modified"]
+    data["tlp"] = pulse["TLP"]
+    data["url"] = f"https://otx.alienvault.com/pulse/{pulse['id']}"
+    return data
+
 def details(pulse_id):
-    return otx.get_pulses_details(pulse_id)
+    return otx.get_pulse_details(pulse_id)
 
 # consulta personalizada por dominio
 def search(term):
