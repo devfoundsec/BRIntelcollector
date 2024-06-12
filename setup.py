d5c90dfac3a1aa9b304583d6e5d85130f88d21d6
m4th <matheusoliveiratux4me@gmail.com>
2024-06-12 16:54:59 -0300

    atualizando o setup :fire:

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="brIntel",
-    version="0.1.1",
+    version="0.1.0",
     description="Ferramenta de coleta de inteligência em websites brasileiros.",
     author=["Matheus Oliveira matheusoliveiratux4me@gmail.com", "Julio Lira jul10l1r4@disroot.org"],
     author_email=["matheusoliveiratux4me@gmail.com", "jul10l1r4@disroot.org"],
@@ -14,6 +14,21 @@ setup(
         "Security",
         "Threat Intelligent",
     ],
+    install_requires=[
+    "requests>=2.26.0",
+    "beautifulsoup4>=4.10.0",
+],
+package_data={
+        'brintelcollector': [
+            'README.md',
+        ],
+    },
+    license="MIT License",
+    entry_points={
+        'console_scripts': [
+            'brintelcollector=brintelcollector.main:main'
+        ]
+    },
     packages=[
         "BRIntel",
     ]
