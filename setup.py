d940d513e0bc1348a8d1d2943e6e9eb30df4a3cd
m4th <matheusoliveiratux4me@gmail.com>
2024-06-12 16:26:06 -0300

    add setup archive :tada:

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="brIntel",
+    version="0.1.1",
+    description="Ferramenta de coleta de inteligência em websites brasileiros.",
+    author=["Matheus Oliveira matheusoliveiratux4me@gmail.com", "Julio Lira jul10l1r4@disroot.org"],
+    author_email=["matheusoliveiratux4me@gmail.com", "jul10l1r4@disroot.org"],
+    url="https://github.com/devfoundsec/BRIntelcollector",
+    classifiers=[
+        "Development Tools",
+        "Science",
+        "Library",
+        "Security",
+        "Threat Intelligent",
+    ],
+    packages=[
+        "BRIntel",
+    ]
+)
