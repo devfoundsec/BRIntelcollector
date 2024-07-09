67a847b59aaf6d1827e956e0a83d7ca2424de136
m4th <matheusoliveiratux4me@gmail.com>
2024-07-09 13:21:40 -0300

    atualizar repositorio :tada:

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 from setuptools import setup
 
 setup(
-    name="brIntel",
+    name="BRIntel1",
     version="0.1.0",
     description="Ferramenta de coleta de inteligência em websites brasileiros.",
     author=["Matheus Oliveira matheusoliveiratux4me@gmail.com", "Julio Lira jul10l1r4@disroot.org"],
     author_email=["matheusoliveiratux4me@gmail.com", "jul10l1r4@disroot.org"],
     url="https://github.com/devfoundsec/BRIntelcollector",
     classifiers=[
-        "Development Tools",
-        "Science",
-        "Library",
-        "Security",
-        "Threat Intelligent",
+        "Intended Audience :: System Administrators",
+        "Programming Language :: Python :: 3",
+        "Topic :: Security",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     install_requires=[
     "requests>=2.26.0",
